8d565a5ba550890d47193083021b99d60319ef7c
Vova Potiichuk <volodymyr.potiichuk@prosapient.com>
2022-10-02 22:22:22 +0300

    feat: add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import { useAppDispatch, useAppSelector } from "store/index";
 import { selectTodos } from "store/todos/slice";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
@@ -9,16 +8,51 @@ type InitialValues = {
   name: string;
 };
 
+const CreateForm = () => {
+  const initialValues: InitialValues = {
+    name: "",
+  };
+  const dispatch = useAppDispatch();
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validate={(values) => {
+        const errors = {} as any;
+        if (values.name.length === 0) errors.name = "name is empty";
+        return errors;
+      }}
+      onSubmit={(values) => {
+        dispatch({ type: "todos/add", payload: { name: values.name } });
+      }}
+    >
+      {({ errors, touched }) => {
+        return (
+          <Form>
+            <Field
+              name="name"
+              placeholder="name"
+              className="rounded border py-1 px-3"
+            />
+            {errors.name && touched.name ? (
+              <div style={{ color: "red" }}>{errors.name}</div>
+            ) : null}
+            <button type="submit" className="border py-1 px-3 rounded">
+              add
+            </button>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
 const Root: NextPage = () => {
   const dispatch = useAppDispatch();
   const todos = useAppSelector(selectTodos);
 
-  const initialValues: InitialValues = {
-    name: "",
-  };
-
   return (
-    <div className={styles.container}>
+    <div className="px-8 text-xl">
       <DragDropContext
         onDragEnd={(result) => {
           if (result.destination) {
@@ -32,29 +66,7 @@ const Root: NextPage = () => {
           }
         }}
       >
-        <Formik
-          initialValues={initialValues}
-          validate={(values) => {
-            const errors = {} as any;
-            if (values.name.length === 0) errors.name = "name is empty";
-            return errors;
-          }}
-          onSubmit={(values) => {
-            dispatch({ type: "todos/add", payload: { name: values.name } });
-          }}
-        >
-          {({ errors, touched }) => {
-            return (
-              <Form>
-                <Field name="name" placeholder="name" />
-                {errors.name && touched.name ? (
-                  <div style={{ color: "red" }}>{errors.name}</div>
-                ) : null}
-                <button type="submit">add</button>
-              </Form>
-            );
-          }}
-        </Formik>
+        <CreateForm />
         <Droppable droppableId="droppable">
           {(provided) => (
             <div ref={provided.innerRef} {...provided.droppableProps}>
@@ -67,6 +79,7 @@ const Root: NextPage = () => {
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
+                        className="flex justify-between"
                       >
                         <div>{todo.name}</div>
                         <button
